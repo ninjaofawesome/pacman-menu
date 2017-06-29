f91828a78006f572c88ea326c936d2e6191c65ec
Hannah Nordgren <hey@hereshannahs.info>
2017-06-29 12:04:42 -0400

    [FEATURE] got players hooked up on homepage

---
+++ src/components/Players/Players.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Players extends Component {
+
+  render(){
+    return <h1> I am the players</h1>;
+  }
+}
+
+export default Players;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Players from '../../components/Players/Players';
 
 class Home extends Component {
   render() {
     return(
       <div className="about">
         <h1>I am home!</h1>
+        <Players />
       </div>
     );
   }
